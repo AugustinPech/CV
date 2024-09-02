f3503a9dfa013672296ed153a07d540b9e17f49d
AugustinPech <80385451+AugustinPech@users.noreply.github.com>
2024-09-02 16:32:26 +0200

    CI in progress trying to run a build command

--- js/contact.js
+++ js/contact.js
@@ -1,28 +1,25 @@
-let form = document.querySelector("#contactForm")
+let form = document.querySelector("#contactForm");
 
-function modalContactForm(block){
-    let newChild=block.appendChild(document.createElement("div"))
-    let modalBox= newChild.appendChild(document.createElement("div"))
-    modalBox.className="modal "
-    modalBox.setAttribute('id',"contactFormModal")
-    let modalContent= modalBox.appendChild(document.createElement("div"))
-    modalContent.className= "modal-content"
-    modalContent.innerHTML=form.innerHTML
-    modalBox.style.display="none"
-    block=document.querySelector("#contactBTN")
-    block.addEventListener("click", ()=>{
-        modalBox.style.display="block"
-    })
-    window.addEventListener("click",(event) =>{
-        if (event.target == modalBox) {
-            modalBox.style.display = "none";
-        }
-    })
+function modalContactForm(block) {
+  let newChild = block.appendChild(document.createElement("div"));
+  let modalBox = newChild.appendChild(document.createElement("div"));
+  modalBox.className = "modal ";
+  modalBox.setAttribute("id", "contactFormModal");
+  let modalContent = modalBox.appendChild(document.createElement("div"));
+  modalContent.className = "modal-content";
+  modalContent.innerHTML = form.innerHTML;
+  modalBox.style.display = "none";
+  block = document.querySelector("#contactBTN");
+  block.addEventListener("click", () => {
+    modalBox.style.display = "block";
+  });
+  window.addEventListener("click", (event) => {
+    if (event.target == modalBox) {
+      modalBox.style.display = "none";
+    }
+  });
 }
 
+function contactMe() {}
 
-function contactMe(){
-
-}
-
-modalContactForm(document.querySelector("#contactBTN"))
+modalContactForm(document.querySelector("#contactBTN"));
